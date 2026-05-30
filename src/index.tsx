47bb7db3739fc0777dd39699fbbe3eab77aa692d
Lucas Li <lucas.peiyangj@gmail.com>
2026-05-30 17:56:16 +1000

    Add tic-tac-toe game with move history

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
 
-import Board from "./App";
+import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <Board />
+    <App />
   </StrictMode>
 );
